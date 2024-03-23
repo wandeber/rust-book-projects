71d9ea2f4366f05a80b02b3c5200865f50aa5a27
wandeber <berthewanderer@gmail.com>
2024-03-23 16:20:56 +0100

    Ignore certain code...

--- hello_cargo/target/debug/hello_cargo.d
+++
@@ -1,1 +0,0 @@
-/Users/wandeber/Documents/Learning/rust-book-projects/hello_cargo/target/debug/hello_cargo: /Users/wandeber/Documents/Learning/rust-book-projects/hello_cargo/src/main.rs
--- hello_cargo/target/release/deps/hello_cargo-01119fee39967534.d
+++
@@ -1,5 +0,0 @@
-/Users/wandeber/Documents/Learning/rust-book-projects/hello_cargo/target/release/deps/hello_cargo-01119fee39967534: src/main.rs
-
-/Users/wandeber/Documents/Learning/rust-book-projects/hello_cargo/target/release/deps/hello_cargo-01119fee39967534.d: src/main.rs
-
-src/main.rs:
