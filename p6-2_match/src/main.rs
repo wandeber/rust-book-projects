9ae2307274b695287ef286e68bf4e000c51a01f1
wandeber <berthewanderer@gmail.com>
2024-04-01 19:56:13 +0200

    if let

--- p6-2_match/src/main.rs
+++ p6-2_match/src/main.rs
@@ -64,4 +64,32 @@ fn main() {
 		7 => remove_fancy_hat(),
 		_ => (), // Any other case will do nothing.
 	}
+
+	// Concise Control Flow with if let
+	let config_max = Some(3u8);
+	match config_max {
+		Some(max) => println!("The maximum is configured to be {}", max),
+		_ => (),
+	}
+	let config_max = Some(3u8);
+	if let Some(max) = config_max {
+		println!("The maximum is configured to be {}", max);
+	}
+
+
+	let coin = Coin::Quarter(UsState::Alaska);
+	let mut count = 0;
+	match coin {
+		Coin::Quarter(state) => println!("State quarter from {:?}!", state),
+		_ => count += 1,
+	}
+
+	let coin = Coin::Quarter(UsState::Alaska);
+	let mut count = 0;
+	if let Coin::Quarter(state) = coin {
+		println!("State quarter from {:?}!", state);
+	}
+	else {
+		count += 1;
+	}
 }
